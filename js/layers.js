441c6acb36ff8617e7cbe86e8ff2c1704b4e76cd
ajchen02 <88434425+ajchen02@users.noreply.github.com>
2022-10-07 09:41:42 +0800

    battle

--- js/layers.js
+++ js/layers.js
@@ -15,7 +15,6 @@ addLayer("stat",
         }
     },
     color: "white",
-    resource: "重置点",
     type: "none",
 
     calcStats:{//在这里面计算属性
@@ -90,6 +89,75 @@ addLayer("stat",
             ],
         },
     },
+    row: "side",
+    layerShown(){return true},
+})
+
+
+addLayer("battle",
+{
+    symbol: "b",
+    position: 0,
+    startData()
+    {
+        return{
+            unlocked: true,
+            points: new ExpantaNum(0),
+        }
+    },
+    color: "white",
+    type: "none",
+
+    bars:{
+        thatBar:{
+            
+        },
+    },
+
+    tabFormat:
+    {
+        "属性":
+        {
+            buttonStyle()
+            {
+                return {"border-radius":"0px"}
+            },
+            content:[
+                "blank",
+                ["display-text",
+                    function() {
+                        return '<text style="color:#FF000099">攻击 : </text><text style="color:#FF0000">'+format(player[this.layer].atk)+'</text>'
+                    },
+                    { "color": "white", "font-size": "32px",}
+                ],
+                ["display-text",
+                    function() {
+                        return '<text style="color:#0000FF99">防御 : </text><text style="color:#0000FF">'+format(player[this.layer].def)+'</text>'
+                    },
+                    { "color": "white", "font-size": "32px",}
+                ],
+                ["display-text",
+                    function() {
+                        return '<text style="color:#00FF0099">生命 : </text><text style="color:#00FF00">'+format(player[this.layer].hp)+'</text>'
+                    },
+                    { "color": "white", "font-size": "32px",}
+                ],
+                ["display-text",
+                    function() {
+                        return '<text style="color:#FF00FF99">速度 : </text><text style="color:#FF00FF">'+format(player[this.layer].spd)+'</text>'
+                    },
+                    { "color": "white", "font-size": "32px",}
+                ],
+                ["display-text",
+                    function() {
+                        return '<text style="color:#FF00FF99">幸运 : </text><text style="color:green">'+format(player[this.layer].luck)+' <br>我草匿名你上个色我不知道哪个颜色好看</text>'
+                    },
+                    { "color": "white", "font-size": "32px",}
+                ],
+            ],
+        },
+    },
+
     row: "side",
     layerShown(){return true},
 })
